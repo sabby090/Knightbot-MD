c383b998ff09c822b12411c60d3bac0357e672bb
sabby090 <spiritualitylawal@gmail.com>
2025-06-28 20:39:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
-const settings = {
-  packname: 'Knight Bot',
+constant settings = {
+  packname: 'dumebi',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "dumebi",
   botOwner: 'spirituality', // Your name
   ownerNumber: '2348075226831', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
